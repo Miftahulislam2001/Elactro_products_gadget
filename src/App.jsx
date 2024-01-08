bc3e8cf4e02992e9bf7a451251ba52a8dc8a8710
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-08 09:52:44 +0600

    Using Headless Modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 import Header from "./components/Header";
 import { Outlet, useLoaderData } from "react-router-dom";
 import Footer from "./components/Footer";
+import Modal from "./components/Modal";
 
 export const productsContext = createContext([]);
 export const cartContext = createContext([]);
@@ -9,6 +10,13 @@ export const cartContext = createContext([]);
 const App = () => {
   const { cartArray, products } = useLoaderData();
   const [cart, setCart] = useState(cartArray);
+  let [isOpen, setIsOpen] = useState(false)
+
+  const cartAlert = sessionStorage.getItem('alert')
+  if(cart.length > 0 && cartAlert !== 'true'){
+    setIsOpen(true)
+    sessionStorage.setItem('alert', true)
+  }
 
   return (
     <productsContext.Provider value={products}>
@@ -18,6 +26,7 @@ const App = () => {
           <Outlet />
         </div>
         <Footer />
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
       </cartContext.Provider>
     </productsContext.Provider>
   );
